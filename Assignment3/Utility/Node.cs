2a4d24b7e95caf62797dc7b42e44552dc82f57f5
chinedu-amaechi <chymez56@yahoo.com>
2024-03-25 10:40:54 -0600

    Add project files.

---
+++ Assignment3/Utility/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class Node
+    {
+        public User user { get; set; }
+        public Node Next { get; set; }
+
+        public Node(User user)
+        {
+            this.user = user;
+            Next = null;
+        }
+    }
+}
---
+++ Assignment3/Utility/SLL.cs
@@ -0,0 +1,216 @@
+﻿using Assignment3.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class SLL : ILinkedListADT
+    {
+        private Node head;
+        private int size;
+
+        public SLL()
+        {
+            head = null;
+            size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            size = 0;
+        }
+
+        public void Add(User value, int index)
+        {
+            if (index < 0 || index > size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (index == 0)
+            {
+                AddFirst(value);
+            }
+            else if (index == size)
+            {
+                AddLast(value);
+            }
+            else
+            {
+                Node newNode = new Node(value);
+                Node current = head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                newNode.Next = current.Next;
+                current.Next = newNode;
+                size++;
+            }
+        }
+
+        public void AddFirst(User value)
+        {
+            Node newNode = new Node(value);
+            newNode.Next = head;
+            head = newNode;
+            size++;
+        }
+
+        public void AddLast(User value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+            size++;
+        }
+
+        public bool Contains(User value)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                if (current.user.Equals(value))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+
+        public User GetValue(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.user;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (index == 0)
+            {
+                RemoveFirst();
+            }
+            else if (index == size - 1)
+            {
+                RemoveLast();
+            }
+            else
+            {
+                Node current = head;
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                current.Next = current.Next.Next;
+                size--;
+            }
+        }
+
+                
+        public void RemoveFirst()
+        {
+            if (IsEmpty())
+            {
+                throw new NotImplementedException("List is empty.");
+            }
+
+            head = head.Next;
+            size--;
+        }
+
+
+        public void RemoveLast()
+        {
+            if (IsEmpty())
+            {
+                throw new NotImplementedException("List is empty.");
+            }
+
+            if (size == 1)
+            {
+                head = null;
+            }
+            else
+            {
+                Node current = head;
+                while (current.Next.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+            }
+            size--;
+        }
+
+        public void Replace(User value, int index)
+        {
+            if (index < 0 || index >= size)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            Node current = head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            current.user = value;
+        }
+
+        public int IndexOf(User value)
+        {
+            Node current = head;
+            int index = 0;
+            while (current != null)
+            {
+                if (current.user.Equals(value))
+                {
+                    return index;
+                }
+                current = current.Next;
+                index++;
+            }
+            return -1;
+        }
+    }
+}
